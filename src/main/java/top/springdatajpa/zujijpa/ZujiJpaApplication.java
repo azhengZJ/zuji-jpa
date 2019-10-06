bd177516cea7c76a0e9e026d22dd076d7567313a
azheng <1009460334@qq.com>
2019-10-06 19:00:11 +0800

    first

---
+++ src/main/java/top/springdatajpa/zujijpa/ZujiJpaApplication.java
@@ -0,0 +1,13 @@
+package top.springdatajpa.zujijpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZujiJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZujiJpaApplication.class, args);
+    }
+
+}
